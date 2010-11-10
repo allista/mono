b31501e24ac71c22ca7d9aeea7d4682976918fdc
Zoltan Varga <vargaz@gmail.com>
2010-11-10 18:23:25 +0100

    When single stepping from a exception throw site, single step to the catch site. Fixes #652590.

--- mono/mini/debugger-agent.c
+++ mono/mini/debugger-agent.c
@@ -241,6 +241,14 @@ typedef struct {
 	 * The current mono_runtime_invoke invocation.
 	 */
 	InvokeData *invoke;
+
+	/*
+	 * The context where single stepping should resume while the thread is suspended because
+	 * of an EXCEPTION event.
+	 */
+	MonoContext catch_ctx;
+
+	gboolean has_catch_ctx;
 } DebuggerTlsData;
 
 /* 
@@ -658,7 +666,7 @@ static void ids_cleanup (void);
 
 static void suspend_init (void);
 
-static void ss_start (SingleStepReq *ss_req, MonoMethod *method, SeqPoint *sp, MonoSeqPointInfo *info, MonoContext *ctx, DebuggerTlsData *tls);
+static void ss_start (SingleStepReq *ss_req, MonoMethod *method, SeqPoint *sp, MonoSeqPointInfo *info, MonoContext *ctx, DebuggerTlsData *tls, gboolean step_to_catch);
 static ErrorCode ss_create (MonoInternalThread *thread, StepSize size, StepDepth depth, EventRequest *req);
 static void ss_destroy (SingleStepReq *req);
 
@@ -4013,7 +4021,7 @@ process_breakpoint_inner (DebuggerTlsData *tls, MonoContext *ctx)
 			g_ptr_array_add (ss_reqs, req);
 
 		/* Start single stepping again from the current sequence point */
-		ss_start (ss_req, ji->method, sp, info, ctx, NULL);
+		ss_start (ss_req, ji->method, sp, info, ctx, NULL, FALSE);
 	}
 	
 	if (ss_reqs->len > 0)
@@ -4372,7 +4380,7 @@ ss_stop (SingleStepReq *ss_req)
  *   Start the single stepping operation given by SS_REQ from the sequence point SP.
  */
 static void
-ss_start (SingleStepReq *ss_req, MonoMethod *method, SeqPoint *sp, MonoSeqPointInfo *info, MonoContext *ctx, DebuggerTlsData *tls)
+ss_start (SingleStepReq *ss_req, MonoMethod *method, SeqPoint *sp, MonoSeqPointInfo *info, MonoContext *ctx, DebuggerTlsData *tls, gboolean step_to_catch)
 {
 	gboolean use_bp = FALSE;
 	int i, frame_index;
@@ -4385,7 +4393,10 @@ ss_start (SingleStepReq *ss_req, MonoMethod *method, SeqPoint *sp, MonoSeqPointI
 	/*
 	 * Implement single stepping using breakpoints if possible.
 	 */
-	if (ss_req->depth == STEP_DEPTH_OVER) {
+	if (step_to_catch) {
+		bp = set_breakpoint (method, sp->il_offset, ss_req->req, NULL);
+		ss_req->bps = g_slist_append (ss_req->bps, bp);
+	} else if (ss_req->depth == STEP_DEPTH_OVER) {
 		frame_index = 1;
 		/*
 		 * Find the first sequence point in the current or in a previous frame which
@@ -4433,6 +4444,8 @@ ss_create (MonoInternalThread *thread, StepSize size, StepDepth depth, EventRequ
 	MonoSeqPointInfo *info = NULL;
 	SeqPoint *sp = NULL;
 	MonoMethod *method = NULL;
+	MonoDebugMethodInfo *minfo;
+	gboolean step_to_catch = FALSE;
 
 	if (suspend_count == 0)
 		return ERR_NOT_SUSPENDED;
@@ -4459,9 +4472,37 @@ ss_create (MonoInternalThread *thread, StepSize size, StepDepth depth, EventRequ
 	g_assert (tls->has_context);
 	ss_req->start_sp = ss_req->last_sp = MONO_CONTEXT_GET_SP (&tls->ctx);
 
-	if (ss_req->size == STEP_SIZE_LINE) {
+	if (tls->has_catch_ctx) {
+		gboolean res;
+		StackFrameInfo frame;
+		MonoContext new_ctx;
+		MonoLMF *lmf = NULL;
+
+		/*
+		 * We are stopped at a throw site. Stepping should go to the catch site.
+		 */
+
+		/* Find the the jit info for the catch context */
+		res = mono_find_jit_info_ext (mono_domain_get (), thread->jit_data, NULL, &tls->catch_ctx, &new_ctx, NULL, &lmf, &frame);
+		g_assert (res);
+		g_assert (frame.type == FRAME_TYPE_MANAGED);
+
+		/*
+		 * Find the seq point corresponding to the landing site ip, which is the first seq
+		 * point after ip.
+		 */
+		sp = find_next_seq_point_for_native_offset (frame.domain, frame.method, frame.native_offset, &info);
+		g_assert (sp);
+
+		method = frame.method;
+
+		step_to_catch = TRUE;
+		/* This make sure the seq point is not skipped by process_single_step () */
+		ss_req->last_sp = NULL;
+	}
+
+	if (!step_to_catch && ss_req->size == STEP_SIZE_LINE) {
 		StackFrame *frame;
-		MonoDebugMethodInfo *minfo;
 
 		/* Compute the initial line info */
 		compute_frame_info (thread, tls);
@@ -4483,7 +4524,7 @@ ss_create (MonoInternalThread *thread, StepSize size, StepDepth depth, EventRequ
 		}
 	}
 
-	if (ss_req->depth == STEP_DEPTH_OVER) {
+	if (!step_to_catch && ss_req->depth == STEP_DEPTH_OVER) {
 		StackFrame *frame;
 
 		compute_frame_info (thread, tls);
@@ -4491,7 +4532,7 @@ ss_create (MonoInternalThread *thread, StepSize size, StepDepth depth, EventRequ
 		g_assert (tls->frame_count);
 		frame = tls->frames [0];
 
-		if (frame->il_offset != -1) {
+		if (!method && frame->il_offset != -1) {
 			/* FIXME: Sort the table and use a binary search */
 			sp = find_prev_seq_point_for_native_offset (frame->domain, frame->method, frame->native_offset, &info);
 			if (!sp) return ERR_NOT_IMPLEMENTED; // This can happen with exceptions when stepping
@@ -4499,7 +4540,7 @@ ss_create (MonoInternalThread *thread, StepSize size, StepDepth depth, EventRequ
 		}
 	}
 
-	ss_start (ss_req, method, sp, info, NULL, tls);
+	ss_start (ss_req, method, sp, info, NULL, tls, step_to_catch);
 
 	return 0;
 }
@@ -4630,7 +4671,15 @@ mono_debugger_agent_handle_exception (MonoException *exc, MonoContext *throw_ctx
 	events = create_event_list (EVENT_KIND_EXCEPTION, NULL, ji, &ei, &suspend_policy, NULL);
 	mono_loader_unlock ();
 
+	if (tls && catch_ctx) {
+		tls->catch_ctx = *catch_ctx;
+		tls->has_catch_ctx = TRUE;
+	}
+
 	process_event (EVENT_KIND_EXCEPTION, &ei, 0, throw_ctx, events, suspend_policy);
+
+	if (tls)
+		tls->has_catch_ctx = FALSE;
 }
 
 /*

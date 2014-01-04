bc236b50094e03aadd3c15caa82bb7e5d1dd023b
Alexander Gavrilov <angavrilov@gmail.com>
2014-01-04 22:28:46 +0400

    Implement statistical profiling with stack traces for windows.

--- mono/mini/mini-windows.c
+++ mono/mini/mini-windows.c
@@ -87,21 +87,85 @@ SIG_HANDLER_SIGNATURE (mono_chain_signal)
 	return TRUE;
 }
 
+#if defined(__i386__) || defined(__x86_64__)
+#define FULL_STAT_PROFILER_BACKTRACE 1
+#define CURRENT_FRAME_GET_BASE_POINTER(f) (* (gpointer*)(f))
+#define CURRENT_FRAME_GET_RETURN_ADDRESS(f) (* (((gpointer*)(f)) + 1))
+#if MONO_ARCH_STACK_GROWS_UP
+#define IS_BEFORE_ON_STACK <
+#define IS_AFTER_ON_STACK >
+#else
+#define IS_BEFORE_ON_STACK >
+#define IS_AFTER_ON_STACK <
+#endif
+#else
+#define FULL_STAT_PROFILER_BACKTRACE 0
+#endif
+
 static HANDLE win32_main_thread;
 static MMRESULT win32_timer;
+static MonoDomain *win32_main_domain;
+static MonoJitTlsData *win32_jit_tls;
 
 static void CALLBACK
 win32_time_proc (UINT uID, UINT uMsg, DWORD dwUser, DWORD dw1, DWORD dw2)
 {
+	int call_chain_depth = mono_profiler_stat_get_call_chain_depth ();
 	CONTEXT context;
+
+	if (win32_main_domain != mono_get_root_domain ())
+		return;
+
+	/* Suspend is required to get consistent data. However, while the thread
+	   is suspended, calling any functions that allocate memory can deadlock. */
+	if (SuspendThread (win32_main_thread) < 0)
+		return;
 
 	context.ContextFlags = CONTEXT_CONTROL;
 	if (GetThreadContext (win32_main_thread, &context)) {
+		guchar *ips[MONO_PROFILER_MAX_STAT_CALL_CHAIN_DEPTH+1];
+
 #ifdef _WIN64
-		mono_profiler_stat_hit ((guchar *) context.Rip, &context);
+		ips[0] = (guchar *) context.Rip;
 #else
-		mono_profiler_stat_hit ((guchar *) context.Eip, &context);
+		ips[0] = (guchar *) context.Eip;
 #endif
+
+		if (call_chain_depth == 0) {
+			ResumeThread (win32_main_thread);
+			mono_profiler_stat_hit (ips[0], &context);
+		} else {
+			int current_frame_index = 1;
+
+#if FULL_STAT_PROFILER_BACKTRACE
+			guchar *stack_bottom = (guchar *) win32_jit_tls->end_of_stack;
+#ifdef _WIN64
+			guchar *stack_top = (guchar *) context.Rsp;
+			guchar *current_frame = (guchar *) context.Rbp;
+#else
+			guchar *stack_top = (guchar *) context.Esp;
+			guchar *current_frame = (guchar *) context.Ebp;
+#endif
+
+			__try {
+				while ((current_frame_index <= call_chain_depth) &&
+						(stack_bottom IS_BEFORE_ON_STACK (guchar*) current_frame) &&
+						((guchar*) current_frame IS_BEFORE_ON_STACK stack_top)) {
+					ips [current_frame_index] = CURRENT_FRAME_GET_RETURN_ADDRESS (current_frame);
+					current_frame_index ++;
+					stack_top = current_frame;
+					current_frame = CURRENT_FRAME_GET_BASE_POINTER (current_frame);
+				}
+			} __except (GetExceptionCode() == EXCEPTION_ACCESS_VIOLATION) {
+				/* Apparently we ended up going through the bottom of the stack - ignore */
+			}
+#endif
+
+			ResumeThread (win32_main_thread);
+			mono_profiler_stat_call_chain (current_frame_index, & ips [0], &context);
+		}
+	} else {
+		ResumeThread (win32_main_thread);
 	}
 }
 
@@ -114,11 +178,19 @@ mono_runtime_setup_stat_profiler (void)
 	if (inited)
 		return;
 
+	win32_main_domain = mono_domain_get ();
+	if (win32_main_domain != mono_get_root_domain ())
+		return;
+
+	win32_jit_tls = TlsGetValue (mono_jit_tls_id);
+	if (win32_jit_tls == NULL)
+		return;
+
 	inited = 1;
 	if (timeGetDevCaps (&timecaps, sizeof (timecaps)) != TIMERR_NOERROR)
 		return;
 
-	if ((win32_main_thread = OpenThread (READ_CONTROL | THREAD_GET_CONTEXT, FALSE, GetCurrentThreadId ())) == NULL)
+	if ((win32_main_thread = OpenThread (READ_CONTROL | THREAD_GET_CONTEXT | THREAD_SUSPEND_RESUME, FALSE, GetCurrentThreadId ())) == NULL)
 		return;
 
 	if (timeBeginPeriod (1) != TIMERR_NOERROR)
